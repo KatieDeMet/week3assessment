0e7aec3b76f642ba8df83ec24987aabdc5334833
Katie DeMet <katie.demet7@gmail.com>
2021-07-23 11:31:23 -0500

    partial extra credit work

---
+++ neighborhood.js
@@ -0,0 +1,7 @@
+let restBtn = document.querySelector('#restaurantBtn');
+let restArray = ["Dockside 'N Duck", "Crispy Duck", "Cravings"]
+function recommend(event) {
+    
+}
+
+restBtn.addEventListener('click', recommend);
